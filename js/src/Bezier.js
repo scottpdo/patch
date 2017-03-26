4dbff9cca9dec3fe376305a898509a7b9c41ad11
Scott Donaldson <scott.p.donaldson@gmail.com>
2017-03-26 13:25:45 -0400

    documentation

--- js/src/Bezier.js
+++ js/src/Bezier.js
@@ -1,6 +1,14 @@
 import Point from './Point';
 
-let Bezier = function(p0, p1, p2, p3) {
+/**
+ * A curve defined by 2, 3, or 4 control points.
+ * @class
+ * @param {Point} p0
+ * @param {Point} p1
+ * @param {Point} p2
+ * @param {Point} p3
+ */
+function Bezier(p0, p1, p2, p3) {
 
     this.p0 = p0;
     this.p1 = p1;
@@ -13,6 +21,11 @@ let Bezier = function(p0, p1, p2, p3) {
 
 };
 
+/**
+ * Evaluate a curve at parameter t, 0-1.
+ * @param {Number} t - t between 0 and 1 (inclusive).
+ * @returns {Point} The point on the curve at parameter t.
+ */
 Bezier.prototype.evaluate = function(t) {
 
     if (t < 0 || t > 1) throw new Error("Can't evaluate this curve.");
@@ -43,24 +56,44 @@ Bezier.prototype.evaluate = function(t) {
     }
 };
 
+/**
+ * Set or get the curve's 0th control point.
+ * @param {Point|Null} pt
+ * @returns {Bezier|Point} If no value given, returns the 0th control point. Otherwise sets it and returns the Bezier object.
+ */
 Bezier.prototype.pt0 = function(pt) {
     if (!pt) return this.p0;
     this.p0 = pt;
     return this;
 };
 
+/**
+ * Set or get the curve's 1st control point.
+ * @param {Point|Null} pt
+ * @returns {Bezier|Point} If no value given, returns the 1st control point. Otherwise sets it and returns the Bezier object.
+ */
 Bezier.prototype.pt1 = function(pt) {
     if (!pt) return this.p1;
     this.p1 = pt;
     return this;
 };
 
+/**
+ * Set or get the curve's 2nd control point.
+ * @param {Point|Null} pt
+ * @returns {Bezier|Point} If no value given, returns the 2nd control point. Otherwise sets it and returns the Bezier object.
+ */
 Bezier.prototype.pt2 = function(pt) {
     if (!pt) return this.p2;
     this.p2 = pt;
     return this;
 };
 
+/**
+ * Set or get the curve's 3rd control point.
+ * @param {Point|Null} pt
+ * @returns {Bezier|Point} If no value given, returns the 3rd control point. Otherwise sets it and returns the Bezier object.
+ */
 Bezier.prototype.pt3 = function(pt) {
     if (!pt) return this.p3;
     this.p3 = pt;
--- js/src/Point.js
+++ js/src/Point.js
@@ -1,50 +1,108 @@
-let Point = function(x, y, z) {
+/**
+ * Point in 3-dimensional space.
+ * @class
+ * @param {Number} x - x value of point. Defaults to 0.
+ * @param {Number} y - y value of point. Defaults to 0.
+ * @param {Number} z - z value of point. Defaults to 0.
+ */
+function Point(x = 0, y = 0, z = 0) {
     this.pt = [x, y, z];
 };
 
+/**
+ * Set or get the point's x value.
+ * @param {Number|Null} x - x value
+ * @returns {Number|Point} If no value given, returns the x value. Otherwise returns the Point object.
+ */
 Point.prototype.x = function(x) {
-    if (!x) return this.pt[0];
+    if (x == null) return this.pt[0];
     this.pt[0] = x;
     return this;
 };
 
+/**
+ * Move the Point in the x direction relative to its current position.
+ * @param {Number} dx - Distance by which to move the Point's x value.
+ * @returns {Point} The point object.
+ */
 Point.prototype.moveX = function(dx) {
     this.pt[0] += dx;
     return this;
 };
 
 Point.prototype.y = function(y) {
-    if (!y) return this.pt[1];
+    if (y == null) return this.pt[1];
     this.pt[1] = y;
     return this;
 };
 
+/**
+ * Move the Point in the y direction relative to its current position.
+ * @param {Number} dy - Distance by which to move the Point's y value.
+ * @returns {Point} The point object.
+ */
 Point.prototype.moveY = function(dy) {
     this.pt[1] += dy;
     return this;
 };
 
 Point.prototype.z = function(z) {
-    if (!z) return this.pt[2];
+    if (z == null) return this.pt[2];
     this.pt[2] = z;
     return this;
 };
 
+/**
+ * Move the Point in the z direction relative to its current position.
+ * @param {Number} dz - Distance by which to move the Point's z value.
+ * @returns {Point} The point object.
+ */
 Point.prototype.moveZ = function(dz) {
     this.pt[2] += dz;
     return this;
 };
 
+/**
+ * Multiply the point by a scalar value s (i.e. scale its position relative to the origin by s).
+ * @param {Number} s - Amount by which to scale the point.
+ * @returns {Point} A new Point object with the updated position.
+ */
 Point.prototype.multiply = function(s) {
     return new Point(this.pt[0] * s, this.pt[1] * s, this.pt[2] * s);
 };
 
+/**
+ * Add the positions of two Points.
+ * @param {Point} pt - The Point to add to the current one.
+ * @returns {Point} A new Point object with the updated position.
+ */
 Point.prototype.add = function(pt) {
     return new Point(
         this.pt[0] + pt.pt[0],
         this.pt[1] + pt.pt[1],
         this.pt[2] + pt.pt[2]
     );
+};
+
+/**
+ * In-place rotation of Point in x-y space (i.e. about z axis).
+ * @param {Number} deg - Number of degrees by which to rotate.
+ * @returns {Point} The original Point object with rotated position.
+ */
+Point.prototype.rotate = function(deg) {
+
+    let x = this.x(),
+        y = this.y(),
+        c = Math.cos(deg * Math.PI / 180),
+        s = Math.sin(deg * Math.PI / 180);
+
+    let nx = c * x - s * y;
+    let ny = s * x + c * y;
+
+    this.x(nx);
+    this.y(ny);
+
+    return this;
 };
 
 export default Point;
